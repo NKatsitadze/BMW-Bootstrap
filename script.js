dfcfa3286d4d5ef60f9dab31d03d11a881ebe794
NKatsitadze <105362448+NKatsitadze@users.noreply.github.com>
2022-10-09 19:38:18 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 'use strict'
 
-// {/* <script>
-//     document.getElementById('vid').play();
-// </script> */}
-
 const videos = document.querySelectorAll('#vid');
 videos.forEach(vid => {
     vid.play();
